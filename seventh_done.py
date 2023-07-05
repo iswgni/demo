e3a93fae577bfa254afeb8db404d8b8c1c9b629c
iswgni <137087912+iswgni@users.noreply.github.com>
2023-07-05 18:20:52 +0500

    Add files via upload

---
+++ seventh_done.py
@@ -0,0 +1,42 @@
+class Animal:
+    num = 0
+    def __init__(self, name, sound='не знаю что'):
+        self.name = name
+        self.sound = sound
+        Animal.num = Animal.num + 1
+
+    def voice(self):
+        pass
+
+
+def printNum():
+    print("Столько - ", Animal.num)
+
+class Dog(Animal):
+    def voice(self):
+        print('%s делает %s' % (self.name, self.sound))
+
+
+class Sova(Animal):
+    def voice(self):
+        print('%s делает %s' % (self.name, self.sound))
+
+
+class Lev(Animal):
+    def __init__(self, name, sound = 'не знаю что'):
+        Animal.__init__(self, name, 'АаАаАаАаАаА')
+
+    def voice(self):
+        print('Хочте я вам покажу як %s рычит - %s' % (self.name, self.sound))
+
+
+dog = Dog('Собака', 'гав-гва')
+dog.voice()
+
+sova = Sova('Сова', 'уу-уу')
+sova.voice()
+
+lev = Lev('Лев')
+lev.voice()
+
+printNum()
